37eb74cdd68b5626509b336159b00b5f2de124ef
Lana Barisic <lana.barisic@fer.hr>
2026-04-06 14:57:21 +0200

    added menu bar on all pages

--- src/main/java/todolist/controller/HomeController.java
+++ src/main/java/todolist/controller/HomeController.java
@@ -1,12 +1,35 @@
 package todolist.controller;
+
+import todolist.authentication.ManagerUserSession;
+import todolist.dto.UsuarioData;
+import todolist.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private ManagerUserSession managerUserSession;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping("/about")
     public String about(Model model) {
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+
+        // If user is logged in, pass user information to the template
+        if (idUsuarioLogeado != null) {
+            UsuarioData usuario = usuarioService.findById(idUsuarioLogeado);
+            model.addAttribute("usuario", usuario);
+            model.addAttribute("estaLogeado", true);
+        } else {
+            model.addAttribute("estaLogeado", false);
+        }
+
         return "about";
     }
 }
